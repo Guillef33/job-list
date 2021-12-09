e828dfca9840e11ba8158682e955729c55140777
Guillermo <guillef33@gmail.com>
2021-12-09 14:39:27 -0300

    cambio de rutas y nombres

--- src/enum/Companies.js
+++ src/enum/Companies.js
@@ -3,7 +3,7 @@ import eyecam from "../components/images/eyecam-co.svg";
 import faceit from "../components/images/faceit.svg";
 import insure from "../components/images/insure.svg";
 import loopStudios from "../components/images/loop-studios.svg";
-import manager from "../components/images/manage.svg";
+import manage from "../components/images/manage.svg";
 import myhome from "../components/images/myhome.svg";
 import photosnap from "../components/images/photosnap.svg";
 import shortly from "../components/images/shortly.svg";
@@ -15,7 +15,7 @@ export const CompanyImages = {
   faceit: faceit,
   insure: insure,
   loopStudios: loopStudios,
-  manager: manager,
+  manage: manage,
   myhome: myhome,
   photosnap: photosnap,
   shortly: shortly,
