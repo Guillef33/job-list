05c4b51af2c95a6a2827f5a7f6da456e71989133
Guillermo <guillef33@gmail.com>
2021-12-09 11:52:00 -0300

    react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import JobList from './components/JobList';
 import SearchBar from './components/SearchBar';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <SearchBar />
-      <JobList />
+      <Routes>
+        <Route path="/" element={<SearchBar />} />
+        <Route path="/jobs" element={<JobList />} />
+      </Routes>
     </div>
   );
 }
